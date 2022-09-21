6b937e4b2006645d1b6e5f241a161c9c89a42160
ochuks1 <cochuks@gmail.com>
2022-09-21 05:53:00 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Challenge-2: Play Fetch
+ This is exercise 2
---
+++ Scripts/PlayerControllerX.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    //time the player waits to spawn dog again
+    private float fireRate = 1;
+    //time of start when player can spawn(fire) dog again
+    private float nextFire = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, if the time has elasped since last fire, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > nextFire)
+        {
+            //reset nextFire(spawn) to current time + fireRate
+            nextFire = Time.time + fireRate;
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+        }
+    }
+}
